bce6f548150d96bd41ec7a5e6c04399ee70f8c43
Ogaitw <tiagoaugusto2402@gmail.com>
2023-04-04 15:57:45 -0300

    Arrays

--- src/Arraysaula1.java
+++ src/Arraysaula1.java
@@ -1,5 +1,9 @@
 public class Arraysaula1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] idades = new int[3];
+        idades[0] = 32;
+        idades[1] = 15;
+        idades[2] = 1;
+        System.out.println( idades[0]+","+ idades[1]+","+idades[2]);
     }
 }
---
+++ src/Arraysaula3.java
@@ -0,0 +1,2 @@
+public class Arraysaula3 {
+}
